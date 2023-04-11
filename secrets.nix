efdbc3b655a79ab6f663469fc632477c2b7b4b87
Roman Zakirzyanov <roz@exante.eu>
2023-04-11 18:48:20 +0300

    add laptop to secrets

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,7 @@
 let
   pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOpDpAsY7nE346rDKku5QmsLGp6QlFTMJrH1X4C2GY0B";
+  laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL9PESjEDCGwgiThiRAZZWIlEniN7mZDjldfvAhlh7n8";
 in {
-  "secrets/wg0-privateKey.age".publicKeys = [ pc ];
-  "secrets/wg1-privateKey.age".publicKeys = [ pc ];
+  "secrets/wg0-privateKey.age".publicKeys = [ pc laptop ];
+  "secrets/wg1-privateKey.age".publicKeys = [ pc laptop ];
 }
