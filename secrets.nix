b20ca85e4a269a82e58ad67c454444203cb8f669
Roman Zakirzyanov <roz@exante.eu>
2023-04-11 19:38:47 +0300

    rekey secrets

--- secrets.nix
+++ secrets.nix
@@ -1,7 +1,9 @@
 let
-  pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOpDpAsY7nE346rDKku5QmsLGp6QlFTMJrH1X4C2GY0B";
+  pc     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOpDpAsY7nE346rDKku5QmsLGp6QlFTMJrH1X4C2GY0B";
   laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL9PESjEDCGwgiThiRAZZWIlEniN7mZDjldfvAhlh7n8";
+  roz    = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdsUJHGS6cO/Xp5r3PqzgDDlx7nCHXzFiE1pjBX6rkN";
+  all = [ pc laptop roz ];
 in {
-  "secrets/wg0-privateKey.age".publicKeys = [ pc laptop ];
-  "secrets/wg1-privateKey.age".publicKeys = [ pc laptop ];
+  "secrets/wg0-privateKey.age".publicKeys = all;
+  "secrets/wg1-privateKey.age".publicKeys = all;
 }
