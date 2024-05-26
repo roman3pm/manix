b7650a12d257a56f5d1860ef918667c894397dc5
Roman Zakirzyanov <roman3pm@yandex.ru>
2024-05-26 19:41:27 +0300

    back to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
