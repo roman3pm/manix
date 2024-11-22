d99f11554198e732625bd7810f84138e6549a62f
Roman Zakirzyanov <roman3pm@yandex.ru>
2024-11-22 23:19:26 +0300

    rekey secrets for laptop

--- secrets.nix
+++ secrets.nix
@@ -1,11 +1,15 @@
 let
-  pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOpDpAsY7nE346rDKku5QmsLGp6QlFTMJrH1X4C2GY0B";
-  laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL9PESjEDCGwgiThiRAZZWIlEniN7mZDjldfvAhlh7n8";
-  roz = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdsUJHGS6cO/Xp5r3PqzgDDlx7nCHXzFiE1pjBX6rkN";
+  roz-pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdsUJHGS6cO/Xp5r3PqzgDDlx7nCHXzFiE1pjBX6rkN";
+  roz-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETTU9unFd79n0ToIig6BO0boOqJ8ImnS9vTCPy4alw7";
+
+  system-pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOpDpAsY7nE346rDKku5QmsLGp6QlFTMJrH1X4C2GY0B";
+  system-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL9PESjEDCGwgiThiRAZZWIlEniN7mZDjldfvAhlh7n8";
+
   all = [
-    pc
-    laptop
-    roz
+    roz-pc
+    roz-laptop
+    system-pc
+    system-laptop
   ];
 in
 {
