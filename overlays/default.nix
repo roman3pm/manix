bf288212541227bf0f37f47a43d94e87ec377fb0
Roman Zakirzyanov <roman3pm@yandex.ru>
2024-11-29 22:40:34 +0300

    remove mutter overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -15,27 +15,4 @@ in
     inherit gen-nvim;
   };
 
-  mutter = prev.mutter.overrideAttrs (old: {
-    src = prev.fetchFromGitLab {
-      domain = "gitlab.gnome.org";
-      owner = "vanvugt";
-      repo = "mutter";
-      rev = "triple-buffering-v4-47";
-      hash = "sha256-JaqJvbuIAFDKJ3y/8j/7hZ+/Eqru+Mm1d3EvjfmCcug=";
-    };
-    preConfigure =
-      let
-        gvdb = final.fetchFromGitLab {
-          domain = "gitlab.gnome.org";
-          owner = "GNOME";
-          repo = "gvdb";
-          rev = "2b42fc75f09dbe1cd1057580b5782b08f2dcb400";
-          hash = "sha256-CIdEwRbtxWCwgTb5HYHrixXi+G+qeE1APRaUeka3NWk=";
-        };
-      in
-      ''
-        cp -a "${gvdb}" ./subprojects/gvdb
-      '';
-  });
-
 }
