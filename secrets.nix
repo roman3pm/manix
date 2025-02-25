3df82cbe2c7ac6df9fb273052165a90be38de359
Roman Zakirzyanov <roman3pm@yandex.ru>
2025-02-25 18:06:21 +0300

    change machine names

--- secrets.nix
+++ secrets.nix
@@ -1,15 +1,15 @@
 let
   roz-pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdsUJHGS6cO/Xp5r3PqzgDDlx7nCHXzFiE1pjBX6rkN";
-  roz-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETTU9unFd79n0ToIig6BO0boOqJ8ImnS9vTCPy4alw7";
+  roz-x1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETTU9unFd79n0ToIig6BO0boOqJ8ImnS9vTCPy4alw7";
 
   system-pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOpDpAsY7nE346rDKku5QmsLGp6QlFTMJrH1X4C2GY0B";
-  system-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILZe4ZoH8BVwAJVQY0D/ZO92/qOJ/3kX3sb1ExhX0ld5";
+  system-x1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILZe4ZoH8BVwAJVQY0D/ZO92/qOJ/3kX3sb1ExhX0ld5";
 
   all = [
     roz-pc
-    roz-laptop
+    roz-x1
     system-pc
-    system-laptop
+    system-x1
   ];
 in
 {
